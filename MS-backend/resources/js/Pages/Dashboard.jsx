d022021bcdae6048b9f1308020ed29fbb207d688
LucaoCorrea <lucascorrea1505@gmail.com>
2025-01-09 11:08:06 -0300

    .

--- MS-backend/resources/js/Pages/Dashboard.jsx
+++ MS-backend/resources/js/Pages/Dashboard.jsx
@@ -9,9 +9,9 @@ export default function Dashboard() {
                     Dashboard
                 </h2>
             }
-        >
+        > 
             <Head title="Dashboard" />
-
+                
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
